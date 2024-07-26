9b3d26fa3df7a5b5d96363113aa8751cd87f7280
Rob Longbottom <robert.longbottom@3db-labs.com>
2024-07-25 21:06:16 -0400

    Cleanup perspectives

--- src/mainview.py
+++ src/mainview.py
@@ -3,15 +3,20 @@ import logging
 from abc import ABCMeta, ABC, abstractmethod
 from typing import Callable
 
-from PySide6.QtCore import Signal, Slot, QCoreApplication, QObject, QSize, QSignalBlocker
+from PySide6.QtCore import Signal, Slot, QCoreApplication, QObject, QSettings
 from PySide6.QtWidgets import QSizePolicy, QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QWidget, \
-    QCheckBox, QPlainTextEdit, QMessageBox, QTableView, QAbstractItemView, QGroupBox, QInputDialog, QComboBox, \
-    QWidgetAction
+    QCheckBox, QPlainTextEdit, QMessageBox, QTableView, QAbstractItemView, QGroupBox, QInputDialog
 from PySide6.QtGui import QFont, QAction
 
 import PySide6QtAds as QtAds
 
 from consultmodel import ConsultInfo, ConsultParameterTableModel
+
+
+def resize_font(font: QFont, point_size: int):
+    f = QFont(font)
+    f.setPointSize(point_size)
+    return f
 
 
 #
@@ -44,14 +49,14 @@ class QtHandler(logging.Handler):
         self.signaller.signal.emit(s, record)
 
 
-# class QABCMeta(ABCMeta, type(QWidget)):
-#     pass
-#
-#
-# class DockableView(ABC, metaclass=QABCMeta):
-#     @abstractmethod
-#     def dock_size(self) -> QSize:
-#         pass
+class QABCMeta(ABCMeta, type(QWidget)):
+    pass
+
+
+class DockableView(ABC, metaclass=QABCMeta):
+    @abstractmethod
+    def initial_expanded_size(self) -> int:
+        pass
 
 
 class TableView(QWidget):
@@ -70,16 +75,17 @@ class TableView(QWidget):
         self.setLayout(layout)
 
 
-class OptionsView(QWidget):
+class OptionsView(QWidget, DockableView):
     def __init__(self, parent):
         super().__init__(parent)
-        layout = QVBoxLayout(self)
+        layout = QHBoxLayout(self)
         param_gb = QGroupBox("ECU Parameters", self)
         param_layout = QVBoxLayout(param_gb)
         for param in ConsultInfo.get_params():
             param_layout.addWidget(self.create_param_checkbox(param))
         layout.addWidget(param_gb)
         self.setLayout(layout)
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
     @staticmethod
     def create_param_checkbox(param):
@@ -87,15 +93,16 @@ class OptionsView(QWidget):
             logging.debug("State changed for param {} to {}".format(ecu_param.name, state))
             ecu_param.enable(state)
         check = QCheckBox(param.name)
-        f = check.font()
-        f.setPointSize(10)
-        check.setFont(f)
+        check.setFont(resize_font(check.font(), 10))
         check.setContentsMargins(0, 0, 0, 0)
         check.stateChanged.connect(lambda state, p=param: param_state_changed(p, state == 2))
         return check
 
+    def initial_expanded_size(self) -> int:
+        return self.layout().layout().sizeHint().width() + 20
 
-class LogView(QPlainTextEdit):
+
+class LogView(QPlainTextEdit, DockableView):
     def __init__(self, parent):
         super().__init__(parent)
         self.setReadOnly(True)
@@ -109,7 +116,6 @@ class LogView(QPlainTextEdit):
         h.setFormatter(formatter)
         logging.getLogger().addHandler(h)
 
-
     @Slot(str, logging.LogRecord)
     def loghandler(self, status, record):
         self.append(status)
@@ -119,12 +125,34 @@ class LogView(QPlainTextEdit):
         self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
         QCoreApplication.processEvents()
 
+    def initial_expanded_size(self) -> int:
+        return self.layout().layout().sizeHint().width()
+
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # init vars
+        self._store_perspective_act = None
+        self._delete_perspective_act = None
+        self._quit_act = None
+        self._about_act = None
+        self._about_qt_act = None
+
+        self._file_menu = None
+        self._view_menu = None
+        self._windows_menu = None
+        self._help_menu = None
+
+        self._table_view = None
+        self._log_view = None
+        self._options_view = None
+
+        self._perspective_settings_file = QSettings("cv_settings.cfg", QSettings.Format.IniFormat)
+
+        # setup dock manager
         QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.FocusHighlighting, True)
         QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.DockAreaHasTabsMenuButton, False)
         QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.OpaqueSplitterResize, True)
@@ -134,27 +162,14 @@ class MainWindow(QMainWindow):
         QtAds.CDockManager.setAutoHideConfigFlag(QtAds.CDockManager.AutoHideHasMinimizeButton, False)
         self._dock_mgr = QtAds.CDockManager(self)
 
-        self._save_perspective_act = None
-        self._perspective_list_act = None
-        self._perspective_combo = None
-        self._quit_act = None
-        self._about_act = None
-        self._about_qt_act = None
+        # load perspectives
+        self._dock_mgr.loadPerspectives(self._perspective_settings_file)
+        self._current_perspective = ""
+
+        # setup main window
         self.create_actions()
-
-        self._file_menu = None
-        self._view_menu = None
-        self._help_menu = None
         self.create_menus()
-
-        self._perspective_tool_bar = None
-        self.create_tool_bars()
-
         self.create_status_bar()
-
-        self._table_view = None
-        self._log_view = None
-        self._options_view = None
         self.create_dock_windows()
 
         self.setWindowTitle("Consult Viewer")
@@ -169,28 +184,6 @@ class MainWindow(QMainWindow):
                           "standard paragraphs to add them.")
 
     def create_actions(self):
-        # icon = QIcon.fromTheme('document-new', QIcon(':/res/new.png'))
-        # self._new_letter_act = QAction(icon, "&New Letter",
-        #                                self, shortcut=QKeySequence.New,
-        #                                statusTip="Create a new form letter",
-        #                                triggered=self.new_letter)
-        #
-        # icon = QIcon.fromTheme('document-save', QIcon(':/res/save.png'))
-        # self._save_act = QAction(icon, "&Save...", self,
-        #                          shortcut=QKeySequence.Save,
-        #                          statusTip="Save the current form letter", triggered=self.save)
-        #
-        # icon = QIcon.fromTheme('document-print', QIcon(':/res/print.png'))
-        # self._print_act = QAction(icon, "&Print...", self,
-        #                           shortcut=QKeySequence.Print,
-        #                           statusTip="Print the current form letter",
-        #                           triggered=self.print_)
-        #
-        # icon = QIcon.fromTheme('edit-undo', QIcon(':/res/undo.png'))
-        # self._undo_act = QAction(icon, "&Undo", self,
-        #                          shortcut=QKeySequence.Undo,
-        #                          statusTip="Undo the last editing action", triggered=self.undo)
-
         self._quit_act = QAction("&Quit",
                                  parent=self,
                                  shortcut="Ctrl+Q",
@@ -206,29 +199,39 @@ class MainWindow(QMainWindow):
                                      statusTip="Show the Qt library's About box",
                                      triggered=QApplication.instance().aboutQt)
 
-        self._save_perspective_act = QAction("Create Perspective", parent=self,
-                                     statusTip="Save the current perspective",
-                                     triggered=self.save_perspective)
+        self._store_perspective_act = QAction("Save",
+                                              parent=self,
+                                              statusTip="Save the current perspective",
+                                              triggered=self.store_perspective)
 
-        self._perspective_list_act = QWidgetAction(self)
-        self._perspective_combo = QComboBox(self)
-        self._perspective_combo.setSizeAdjustPolicy(QComboBox.SizeAdjustPolicy.AdjustToContents)
-        self._perspective_combo.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
-        self._perspective_combo.currentTextChanged.connect(self._dock_mgr.openPerspective)
-        self._perspective_list_act.setDefaultWidget(self._perspective_combo)
+        self._delete_perspective_act = QAction("Delete",
+                                               parent=self,
+                                               statusTip="Remove a perspective",
+                                               triggered=self.delete_perspective)
 
     def create_menus(self):
         self._file_menu = self.menuBar().addMenu("&File")
-        # self._file_menu.addAction(self._new_letter_act)
-        # self._file_menu.addAction(self._save_act)
-        # self._file_menu.addAction(self._print_act)
-        # self._file_menu.addSeparator()
         self._file_menu.addAction(self._quit_act)
-
-        # self._edit_menu = self.menuBar().addMenu("&Edit")
-        # self._edit_menu.addAction(self._undo_act)
-
         self._view_menu = self.menuBar().addMenu("&View")
+        perspective_menu = self._view_menu.addMenu("Perspectives")
+
+        def refresh_perspective_actions():
+            def handle_perspective_selected(name):
+                self._current_perspective = name
+                logging.info(f"Loading perspective '{name}'")
+                self._dock_mgr.openPerspective(name)
+            perspective_menu.clear()
+            for perspective_name in self._dock_mgr.perspectiveNames():
+                action = QAction(perspective_name, self, statusTip=f"Load the '{perspective_name}' perspective")
+                action.triggered.connect(lambda checked, n=perspective_name: handle_perspective_selected(n))
+                perspective_menu.addAction(action)
+            perspective_menu.addSeparator()
+            perspective_menu.addAction(self._store_perspective_act)
+            perspective_menu.addAction(self._delete_perspective_act)
+
+        perspective_menu.aboutToShow.connect(refresh_perspective_actions)
+        self._view_menu.addSeparator()
+        self._windows_menu = self._view_menu.addMenu("Windows")
 
         self.menuBar().addSeparator()
 
@@ -236,51 +239,57 @@ class MainWindow(QMainWindow):
         self._help_menu.addAction(self._about_act)
         self._help_menu.addAction(self._about_qt_act)
 
-    def create_tool_bars(self):
-        pass
-        self._perspective_tool_bar = self.addToolBar("Perspective")
-        self._perspective_tool_bar.addAction(self._perspective_list_act)
-        self._perspective_tool_bar.addAction(self._save_perspective_act)
-
-    #     self._file_tool_bar.addAction(self._new_letter_act)
-    #     self._file_tool_bar.addAction(self._save_act)
-    #     self._file_tool_bar.addAction(self._print_act)
-    #
-    #     self._edit_tool_bar = self.addToolBar("Edit")
-    #     self._edit_tool_bar.addAction(self._undo_act)
-
     def create_status_bar(self):
         self.statusBar().showMessage("Ready")
 
-    def save_perspective(self):
+    def store_perspective(self):
         name, entered = QInputDialog.getText(self, "Save Perspective", "Enter unique name:")
         if not entered or len(name) == 0:
             return
 
         self._dock_mgr.addPerspective(name)
-        QSignalBlocker(self._perspective_combo)
-        self._perspective_combo.clear()
-        self._perspective_combo.addItems(self._dock_mgr.perspectiveNames())
-        self._perspective_combo.setCurrentText(name)
+        logging.info(f"Added perspective '{name}'")
+        self._dock_mgr.savePerspectives(self._perspective_settings_file)
+
+    def delete_perspective(self):
+        perspective_names = self._dock_mgr.perspectiveNames()
+        if len(perspective_names) <= 1:
+            return
+
+        try:
+            current = perspective_names.index(self._current_perspective)
+        except ValueError:
+            current = 0
+
+        selected, ok = QInputDialog.getItem(self, "Delete Perspective", "Select perspective to delete:",
+                                            self._dock_mgr.perspectiveNames(),
+                                            current=current,
+                                            editable=False)
+        if ok:
+            self._dock_mgr.removePerspective(selected)
+            logging.info(f"Removed perspective '{selected}'")
+            self._dock_mgr.savePerspectives(self._perspective_settings_file)
 
     def create_dock_windows(self):
-        def create_and_dock_view(title, area, instantiate: Callable[[QtAds.CDockWidget], QWidget]):
+        def create_and_dock_view(title, area, instantiate: Callable[[QtAds.CDockWidget], DockableView]):
             dock = QtAds.CDockWidget(title, self)
-            # dock.setAllowedAreasQt.DockWidgetArea.LeftDockWidgetArea | Qt.DockWidgetArea.RightDockWidgetArea)
             dockable = instantiate(dock)
             dock.setWidget(dockable)
-            dock.setMinimumSizeHintMode(QtAds.CDockWidget.MinimumSizeHintFromDockWidget)
-            dock.resize(800, 800)
-            dock.setMinimumSize(800, 800)
+            # dock.setMinimumSizeHintMode(QtAds.CDockWidget.MinimumSizeHintFromDockWidget)
+            dock.setMinimumSizeHintMode(QtAds.CDockWidget.MinimumSizeHintFromContent)
             if isinstance(area, QtAds.DockWidgetArea):
                 self._dock_mgr.addDockWidget(area, dock)
             else:
-                self._dock_mgr.addAutoHideDockWidget(area, dock)
-            self._view_menu.addAction(dock.toggleViewAction())
+                container = self._dock_mgr.addAutoHideDockWidget(area, dock)
+                container.setSize(dockable.initial_expanded_size())
+                container.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+
+            self._windows_menu.addAction(dock.toggleViewAction())
 
         # set the table view as the central widget (the main view)
         table_dock = QtAds.CDockWidget("Table", self)
         table_dock.setWidget(TableView(table_dock))
+        table_dock.setMinimumSizeHintMode(QtAds.CDockWidget.MinimumSizeHintFromContent)
         self._dock_mgr.setCentralWidget(table_dock)
 
         create_and_dock_view("Options", QtAds.SideBarRight, lambda d: OptionsView(d))
@@ -304,7 +313,6 @@ if __name__ == "__main__":
     sys.excepthook = handle_exception
 
     window = MainWindow()
-
     window.resize(1600, 900)
     window.show()
 
